9559ffa49929ff3e36e83b24cfd64f8ef737c5c4
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-02-27 20:24:11 +0100

    Create first.js

---
+++ task from 1 to 9 /taklif 02/first.js
@@ -0,0 +1,9 @@
+const headingEl = document.createElement('h1');
+headingEl.textContent='Elzero';
+
+headingEl.style.color='blue';
+headingEl.style.fontSize='80';
+headingEl.style.fontWeight = "bold";
+headingEl.style.textAlign = "center";
+headingEl.style.fontFamily = "Arial";
+document.body.appendChild(headingEl); // this code to add it in the body section 
