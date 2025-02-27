73e90540980f6ca0ef75f865cbb4ff565495d60b
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-02-27 22:20:26 +0100

    Create challengeVideo17.js

---
+++ challengeVideo17.js
@@ -0,0 +1,14 @@
+// chanllenge video 17
+let varOne = "Elzero", varTwo ="Elzero Web School", varThree="25//10";
+let divVar =`
+    <div>
+        <h3>${varOne}</h3>
+        <p>${varTwo}</p>
+        <span>${varThree}</span>
+    </div>
+`
+/*document.write(divVar)
+document.write(divVar)
+document.write(divVar)
+document.write(divVar)*/
+document.write(divVar.repeat(4))
