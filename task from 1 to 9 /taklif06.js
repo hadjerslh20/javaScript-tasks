e818cfa5ed71bd1764ba108dbe662a144b583fbe
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-02-27 20:54:44 +0100

    Create taklif06.js

---
+++ task from 1 to 9 /taklif06.js
@@ -0,0 +1,8 @@
+// first method using multiple line comment
+/*
+console.log("Iam In Console");
+document.write("Iam In Page");*/
+
+// second method using single line comment
+//console.log("Iam In Console");
+//document.write("Iam In Page");
