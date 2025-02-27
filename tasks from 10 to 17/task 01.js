daaf602ab1aa84f681415383353cc7e6229ea3c1
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-02-27 22:52:20 +0100

    Create task 01.js

---
+++ tasks from 10 to 17/task 01.js
@@ -0,0 +1,8 @@
+let numberOne=10;
+let numberTwo=20;
+console.log('The result:'+numberOne+numberTwo)
+console.log('The data type :'+typeof(numberOne+''+numberTwo))
+console.log(`The result:${numberOne}${numberTwo}`)
+console.log(`The data type :${typeof(numberOne+''+numberTwo)}`)
+console.log(numberTwo+'\n'+numberOne)
+console.log(`${numberTwo}\n${numberOne}`)
