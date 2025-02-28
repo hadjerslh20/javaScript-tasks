6bf77511cc40bf8b5d9f969200206c6566d2b18b
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-02-28 17:17:20 +0100

    Create task02.js

---
+++ Tasks from 18 to 22/task02.js
@@ -0,0 +1,19 @@
+let num = 3;
+
+// Solution One
+console.log(num + +true*3); 
+
+// Solution Two
+console.log(num + +"3"); 
+
+// Soultion Three
+console.log(18/3); // 6
+
+// Soultion Four
+console.log(num + +true + +true + +true); // 6
+
+// Solution Five
+console.log(num*2); // 6
+
+// Solution Six
+console.log(num * +'2'); // 6
