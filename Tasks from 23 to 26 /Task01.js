37ee1bba8cab28c772500efb5b3c2366603ed07a
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-03-01 19:43:56 +0100

    Create Task01.js

---
+++ Tasks from 23 to 26 /Task01.js
@@ -0,0 +1,10 @@
+console.log(1e5); // 100000
+console.log(10**5); // 100000
+console.log(25e3 * 4); // 100000
+console.log(25e3 + 25e3 + 25e3 + 25e3); // 100000
+console.log(1e4 * 10); // 100000
+console.log(1e3 * 1e2); // 100000
+console.log(100_000); // 100000
+console.log(100**2 *10); // 100000
+console.log(10*10*10*10*10); // 100000
+console.log(25e3 + 25e3 +5e4); // 100000
