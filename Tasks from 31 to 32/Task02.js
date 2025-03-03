ba1aa4c19bf3627b7ef168638f76d7c7652c45cd
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-03-03 02:18:19 +0100

    Create Task02.js

---
+++ Tasks from 31 to 32/Task02.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 < num2); // true
+console.log(num1 != num2); // true
+console.log(num1 > 8 && num2 >15 ); // true
+console.log(num1 > 8 || num2 >20 ); // true
+console.log(true || num1); // true
+console.log(true || num2); // true
