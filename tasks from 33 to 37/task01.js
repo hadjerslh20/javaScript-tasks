56b6dddd91fd5082af957dde8c66fc8edc28d3c1
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-03-21 15:36:53 +0100

    Create task01.js

---
+++ tasks from 33 to 37/task01.js
@@ -0,0 +1,9 @@
+// task 01
+let num = 220;
+if (num<10) {
+    console.log('00'+ num.toString());
+} else if (num > 10 && num <100 ) {
+    console.log(('0'+ num.toString()));
+} else{
+    console.log((num.toString()));
+}
