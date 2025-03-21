dda3e395f77cee67510118b85b09e2fc2bb86468
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-03-21 18:28:19 +0100

    Create challengeVideo47.js

---
+++ challengeVideo47.js
@@ -0,0 +1,19 @@
+// challenge 
+
+let zero = 0;
+
+let counter = 3;
+
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+
+// Write Code Here
+
+console.log(my.slice(0,4).reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
+
+console.log(my.slice(1,3).reverse()); // ["Elham", "Mazero"]
+str1 = my[1].substring(2,6)
+str2 = my[2].substring(0,2)
+console.log(str2+str1) // "Elzero"
+str3 = my[1].substring(4,5)
+str4 = my[3].substring(0,1)
+console.log(str3+str4); // "rO"
