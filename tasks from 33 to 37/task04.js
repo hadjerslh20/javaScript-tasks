487f0b06bfc2ebecf652e537a8e796826ae496eb
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-03-21 23:39:14 +0100

    Create task04.js

---
+++ tasks from 33 to 37/task04.js
@@ -0,0 +1,63 @@
+// Edit What You Want Here
+
+let num1 = 2;
+let num2 = 1;
+let num3 = 2;
+let num4 = 24;
+
+
+// Condition 1
+
+if (num1 > num2) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// Condition 2
+
+if (num1 > num2 && num1 < num4) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// Condition 3
+
+if (num1 > num2 && num1 === num3) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// Condition 4
+
+if ((num1 + num2) < num4) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// Condition 5
+
+if ((num1 + num3) < num4) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// Condition 6
+
+if ((num1 + num2 + num3) < num4) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// Condition 7
+
+if (num4 - (num1 + num3) + num2 === 21) {
+    console.log("True");
+} else {
+    console.log("False");
+}
