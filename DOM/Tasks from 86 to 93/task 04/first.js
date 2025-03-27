08fbeadb1c688ed7242b1db05ef99a693bcb020b
Hadjer Si Salah <67709438+hadjerslh20@users.noreply.github.com>
2025-03-27 03:08:59 +0100

    Create first.js

---
+++ DOM/Tasks from 86 to 93/task 04/first.js
@@ -0,0 +1,8 @@
+let firstDiv = document.querySelector('.one');
+let secondDiv = document.querySelector('.two')
+console.log(firstDiv);
+console.log(secondDiv);
+firstDiv.setAttribute('title','one');
+firstDiv.textContent='One';
+secondDiv.setAttribute('title','two');
+secondDiv.textContent='Two';
